61d45e9fab76d7240ba4254f95292e97e048eadd
rampreetham <rampreethamkanchi@gmail.com>
2024-03-12 19:20:41 +0530

    registration error fixed

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .forms import SignUpForm
 # Create your views here.
 def category(request,foo):
@@ -46,12 +47,13 @@ def logout_user(request):
 def register_user(request):
     form = SignUpForm()
     if request.method == "POST":
+        print('post requested....')
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-            user=authenticate(request,username=username,password=password)
+            user=authenticate(username=username,password=password)
             login(request,user)
             messages.success(request,("you have Registered ..."))
             return redirect('home')
